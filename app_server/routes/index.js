9a76190666e55cab90265e5927e88db2addc9244
ilabartenev <ilia.bartenev@novardis.com>
2022-06-07 17:40:03 +0300

    added husky pre commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+// eslint-disable-next-line new-cap
 const router = express.Router();
 const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
