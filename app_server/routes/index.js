701f856f24d81e031f03bfed85b650c21e7409db
ilabartenev <ilia.bartenev@novardis.com>
2022-06-07 14:19:44 +0300

    * added views * change routes * work with controllers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('../controllers/main');
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* GET locations page. */
+router.get('/', ctrlLocations.homeList);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* GET others page. */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
