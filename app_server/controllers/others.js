cf853c2f6a6ad80ed708d752ae26ee5430f32c5a
ilabartenev <ilia.bartenev@novardis.com>
2022-06-07 16:58:33 +0300

    Move date into controllers

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,3 +1,9 @@
 module.exports.about = function (_req, res) {
-    res.render('generic-text', { title: 'About' });
+    res.render('generic-text', {
+        title: 'About',
+        textContent: [
+            'MEAN was created to help people find places to sit down and get a bit work done.',
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ea eaque, ex fuga minus, natus nisi perferendis porro quasi quis sit sunt voluptatem voluptates. Hic ipsum quam sit soluta suscipit!'
+        ]
+    });
 };
